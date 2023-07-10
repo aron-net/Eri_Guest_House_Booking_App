4774c8a4482adc3bb960c50a641d74a17452e5a6
aron-helu <aronmilahela@gmail.com>
2023-07-11 01:22:13 +0300

    Create user loging in golang

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello Aron")
+    connectionString := "postgres://postgres:aronmilu@localhost:5432/user_data"
+
+    db, err := sql.Open("postgres", connectionString)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    defer db.Close()
+
+    // Get user input
+    var name string
+    fmt.Print("Enter your name: ")
+    fmt.Scanf("%s", &name)
+
+    var email string
+    fmt.Print("Enter your email: ")
+    fmt.Scanf("%s", &email)
+
+    var password string
+    fmt.Print("Enter your password: ")
+    fmt.Scanf("%s", &password)
+
+    // Create a new user
+    stmt, err := db.Prepare("INSERT INTO users (name, email, password) VALUES ($1, $2, $3)")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    _, err = stmt.Exec(name, email, password)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    // Print success message
+    fmt.Println("User created successfully")
 }
